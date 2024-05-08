f5a8620afb4ed19b5e43b9670530cd64e4dc45bf
ebarronteodoro <ebarronteodoro@hotmail.com>
2024-05-08 02:04:33 -0500

    Experience design commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				changeBorderRadius: {
+					'0%': { borderRadius: '60% 40% 30% 70%/60% 30% 70% 40%' },
+					'100%': { borderRadius: '40% 60% 70% 30%/40% 70% 30% 60%' }
+				}
+			},
+
+			animation: {
+				"change-border-radius": "changeBorderRadius 3s ease-in-out infinite alternate"
+			}
+		},
 	},
 	plugins: [],
 }
